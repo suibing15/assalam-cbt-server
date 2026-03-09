a8600a03a85c9347962b19a2ed571579c580ccb9
Suleiman Ibrahim Inuwa <suibingitservces@gmail.com>
2026-03-09 12:06:21 +0100

    Delete public directory

--- public/assets/js/exam-init.js
+++
@@ -1,8 +0,0 @@
-// public/assets/js/exam-init.js
-document.addEventListener("DOMContentLoaded", () => {
-  handleLogin("authForm", "/api/portal/exam/auth", () => {
-    document.getElementById("auth").style.display = "none";
-    document.getElementById("classSelect").style.display = "block";
-    if (typeof loadClasses === "function") loadClasses();
-  });
-});
